8d5c98d48161b98f0d5ba8a5247d75017857ca70
rancheta <rafaelrancheta@gmail.com>
2020-06-13 13:19:02 -0700

    design touchups and better logic for and or statements

--- src/SendTags.js
+++ src/SendTags.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './SendTags.css';
 
 // { 'Human Torch': ['hero', 'mutant', 'tough', 'dumb', 'tall'], \
 // 'Spiderman': ['hero', 'tough', 'smart', 'tall'], 'Kyle': ['human', 'weak', 'smart', 'short'], 
@@ -11,6 +12,9 @@ export default function SendTags () {
     const [sendTo, updateSendTo] = useState("")
     const [sendType, updateSendType] = useState("")
     const [sent, updateSent] = useState(false)
+    const [error, updateError] = useState("")
+    const [errorMsg, updateErrorMsg] = useState("")
+    const errorTimeout = 3000;
 
     const handleChange = (event) => {
         const value = event.target.value
@@ -20,6 +24,7 @@ export default function SendTags () {
                 updateTags(formattedTags)
                 return
             case "config":
+                // Semi confident JSON parsing
                 try{
                     let reformedVal = value.replace(/“/g, '"')
                                             .replace(/”/g, '"')
@@ -30,6 +35,7 @@ export default function SendTags () {
                 } catch (e) {
                     console.error(e);
                     console.error("Invalid JSON");
+                    showError("Invalid person json");
                     updateConfig({})
                     return
                 }
@@ -51,69 +57,126 @@ export default function SendTags () {
         }
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(tags);
-        console.log(config);
-        console.log(sendTo);
-        console.log(sendType);
+    const refreshError = () => {
+        setTimeout( () => {
+            updateError(false)
+        }, errorTimeout)
+    }
 
-        let sendList = [];
-        let sendToCount = sendTo.length - 1;
+    const showError = (msg) => {
+        updateError(true);
+        refreshError();
+        updateErrorMsg(msg);
+    }
+
+    const validateInput = () => {
+        const hypotheticalLoggerOn = true;
+        if (hypotheticalLoggerOn) {
+            console.log("Tags ", tags);
+            console.log("People ", config);
+            console.log("Send To ", sendTo);
+            console.log("Send Type ", sendType);
+        }
 
         const peopleKeys = Object.keys(config);
+
+        if (!tags || tags.length == 0) {
+            showError("No tags provided");
+            return false;
+        }
+        if (!config) {
+            showError('No people specified');
+            return false;
+        }
+        if (!sendTo || sendTo.length == 0) {
+            showError('No send to tags specified');
+            return false;
+        }
+        if (!sendType || (sendType != "AND" && sendType != "OR") ) {
+            showError('No send type specified');
+            return false;
+        }
+        return true;
+    }
+
+    const hasAllNeededTags = (personTags) => {
+        for (var i = 0; i < sendTo.length; i++) {
+            let neededTag = sendTo[i];
+            if (!personTags.includes(neededTag)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const hasOneNeededTag = (personTags) => {
+        for (var i = 0; i < tags.length; i++) { // Iterate over possible tags
+            let foundCount = 0;
+            let tag = tags[i]
+            if (personTags.includes(tag) && sendTo.includes(tag) ) {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        updateSent(false);
+
+        if(!validateInput())
+            return;
+
+        let sendList = [];
+        let sendToCount = sendTo.length;
+        const peopleKeys = Object.keys(config);
+
         peopleKeys.forEach( (key, index) => { // Iterate over possible recipients
             let person = config[key];
-            for (var i = 0; i < tags.length; i++) { // Iterate over possible tags
-                let foundCount = 0;
-                let tag = tags[i]
-                if (sendType === "OR") {
-                    if (person.includes(tag) && sendTo.includes(tag) ) {
-                        sendList.push(key);
-                        break;
-                    }
-                }
-                else if (sendType === "AND") {
-                    if (person.includes(tag) && sendTo.includes(tag) ) {
-                        foundCount++
-                    }
-                    if (foundCount >= sendToCount) {
-                        sendList.push(key);
-                        break;
-                    }
-                }
+            if (sendType === "OR" && hasOneNeededTag(person)) {
+                sendList.push(key);
+            } else if (sendType === "AND"  && hasAllNeededTags(person)) {
+                sendList.push(key);
             }
         })
+
         updateRecipients(sendList.join(", "));
-        updateSent(true);
-        console.log("Send List", sendList)
+
+        if(sendList.length > 0){
+            updateSent(true);
+        } else {
+            showError('No people match those tags');
+        }
 
     }
 
     return (
-        <div>
+        <div className="SendTags">
+            { error && <div className="SendTags-error">Whoops: {errorMsg}</div> }
             <form onSubmit={handleSubmit} style={{textAlign: "left"}}>
                 <label style={{paddingRight: "10px"}}>
-                    <div>
-                        <span style={{paddingRight: "10px"}}>Tags (separated by commas):</span>
-                        <input type="text" name="tags" onChange={handleChange}/>
+                    <div className="formRow">
+                        <span style={{paddingRight: "10px"}}>Acceptable Tags:</span>
+                        <br />
+                        <input type="text" name="tags" placeholder="Hero, tough, mutant" onChange={handleChange}/>
                     </div>
-                    <div>
-                        <span style={{paddingRight: "10px", paddingTop: "20px"}} 
-                              dangerouslySetInnerHTML={{__html: "People Configs (e.g. {'Spiderman': ['hero', 'tough', 'smart”, 'tall']})"}}>
-                        </span>
-                        <input type="text" name="config" style={{width: '500px'}} onChange={handleChange}/>
+                    <div className="formRow">
+                        <p style={{margin: '0px 0 4px 0'}} > People's Tags </p>
+                        <textarea placeholder="{'Spiderman': ['hero', 'tough', 'smart”, 'tall']}" 
+                                    type="text" name="config" style={{width: '100%', height: '70px'}} onChange={handleChange}
+                            />
                     </div>
-                    <div>
+                    <div className="formRow">
                         <span style={{paddingRight: "10px", paddingTop: "20px"}}>Send To:</span>
-                        <input type="text" name="sendTo" onChange={handleChange}/>
+                        <input type="text" placeholder="Hero, tough" name="sendTo" onChange={handleChange}/>
                     </div>
-                    <div>
+                    <div className="formRow">
                         <span style={{paddingRight: "10px", paddingTop: "20px"}}>AND/OR?: </span>
-                        <input type="text" name="sendType" onChange={handleChange}/>
+                        <input type="text" name="sendType" placeholder="AND/OR (default AND)" onChange={handleChange}/>
                     </div>
                 </label>
-                <input type="submit" value="Send Messages" />
+                <button className="submitButton" type="submit">Send Messages</button>
             </form>
             { sent && <div>Sent to: {recipients}</div> }
         </div>
